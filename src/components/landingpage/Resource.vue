e6d96239ee45f10734c45da6745d44ed8da9d84b
shabai517 <shabai517@163.com>
2017-12-08 16:10:03 +0000

    resouce demo

---
+++ src/components/landingpage/Resource.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="resource-container">
+    <Carousel v-model="value1" loop>
+        <CarouselItem>
+            <div class="demo-carousel">1</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">2</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">3</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">4</div>
+        </CarouselItem>
+    </Carousel>
+  </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                value1: 0
+            }
+        }
+    }
+</script>
+<style scoped>
+    .resource-container{
+        width:100%;
+        height: 100%;
+    }
+    .demo-carousel {
+    height: 200px;
+    line-height: 200px;
+    text-align: center;
+    color: #fff;
+    font-size: 20px;
+    background: #506b9e;
+}
+</style>
