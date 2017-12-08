3770dd7d742403cd4c3fbc79389c3ff63de9a9be
shabai517 <shabai517@163.com>
2017-12-08 16:09:33 +0000

    search Panel initialize

---
+++ src/components/landingpage/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="index-container">
+      <div class="panel search"><SearchPanel/></div>
+      <div class="panel"><Resource/></div>
+  </div>
+</template>
+
+<script>
+  import SearchPanel from '@/components/landingpage/SearchPanel'
+  import Resource from '@/components/landingpage/Resource'
+  export default {
+    name: 'index',
+    data(){
+      return {
+   
+      }
+    },
+    methods:{
+    
+    },
+    components: {
+      SearchPanel,
+      Resource
+    },
+    mounted: function(){
+      
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .panel{
+    position: relative;
+    height: 300px;
+  }
+  .panel.search{
+    height: 150px;
+  }
+</style>
---
+++ src/components/landingpage/SearchPanel.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="search-panel-container">
+        <Input v-model="keyword" class="search-input">
+            <Select v-model="selected" slot="prepend" style="width: 80px">
+                <Option value="archive">Archive</Option>
+                <Option value="cluster">Cluster</Option>
+            </Select>
+            <Button slot="append" icon="ios-search"></Button>
+        </Input>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                keyword:'',
+                selected: 'archive',
+                value12: '',
+                value13: '',
+                select1: 'http',
+                select2: 'com',
+                select3: 'day'
+            }
+        }
+    }
+</script>
+<style scoped>
+    .search-panel-container{
+        width:100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: #F0F0F0;
+    }
+    .search-input{
+        width:500px;
+    }
+</style>
