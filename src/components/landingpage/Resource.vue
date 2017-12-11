c051274aa10fd6278968b7202e8832e275793d2f
shabai517 <shabai517@163.com>
2017-12-11 15:58:14 +0000

    add resource panel

--- src/components/landingpage/Resource.vue
+++ src/components/landingpage/Resource.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="resource-container">
-    <Carousel v-model="value1" loop>
+    <Carousel 
+        v-model="value1" 
+        loop
+        :dots="setting.dots">
         <CarouselItem>
             <div class="demo-carousel">1</div>
         </CarouselItem>
@@ -14,13 +17,20 @@
             <div class="demo-carousel">4</div>
         </CarouselItem>
     </Carousel>
+    <div class="button-container">
+        <Button class="resource-button">Cluster</Button>
+        <Button class="resource-button">Archive</Button>
+    </div>
   </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                value1: 0
+                value1: 0,
+                setting: {
+                    dots: 'none',
+                }
             }
         }
     }
@@ -29,13 +39,25 @@
     .resource-container{
         width:100%;
         height: 100%;
+        position: relative;
     }
     .demo-carousel {
-    height: 200px;
-    line-height: 200px;
-    text-align: center;
-    color: #fff;
-    font-size: 20px;
-    background: #506b9e;
-}
+        height: 100%;
+        line-height: 300px;
+        text-align: center;
+        color: #fff;
+        font-size: 20px;
+        background: #506b9e;
+    }
+    .button-container{
+        position: absolute;
+        bottom:30%;
+        left: 50%;
+        margin-left: -182px;
+    }
+    .resource-button{
+        padding: 12px 30px 14px 30px;
+        font-size: 18px;
+        margin: 0 30px;
+    }
 </style>
