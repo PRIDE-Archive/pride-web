4d7474fa0e1a19a2d7d6e07ca2b4b5b1a247a573
shabai517 <shabai517@163.com>
2017-12-11 15:59:25 +0000

    add statistics panel

---
+++ src/components/landingpage/Statistics.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="statistics-container">
+    <Carousel 
+        v-model="value1" 
+        loop
+        :dots="setting.dots">
+       
+        <CarouselItem class="carousel-container">
+            <Row class="row">
+                <Col span="6">
+                    <div class="item-container">
+                        <div class="item">
+                            <div class="title">Repository</div>
+                            <div class="content">200</div>
+                        </div>
+                    </div>
+                </Col>
+                <Col span="6">
+                    <div class="item-container">
+                        <div class="item">
+                            <div class="title">Repository</div>
+                            <div class="content">200</div>
+                        </div>
+                    </div>
+                </Col>
+                <Col span="6">
+                    <div class="item-container">
+                        <div class="item">
+                            <div class="title">Repository</div>
+                            <div class="content">200</div>
+                        </div>
+                    </div>
+                </Col>
+                <Col span="6">
+                    <div class="item-container">
+                        <div class="item">
+                            <div class="title">Repository</div>
+                            <div class="content">200</div>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </CarouselItem>
+        <CarouselItem>
+            <Row class="row">
+                <Col span="6">
+                    <div class="item-container">
+                        <div class="item">
+                            <div class="title">Repository</div>
+                            <div class="content">200</div>
+                        </div>
+                    </div>
+                </Col>
+                <Col span="6">
+                    <div class="item-container">
+                        <div class="item">
+                            <div class="title">Repository</div>
+                            <div class="content">200</div>
+                        </div>
+                    </div>
+                </Col>
+                <Col span="6">
+                    <div class="item-container">
+                        <div class="item">
+                            <div class="title">Repository</div>
+                            <div class="content">200</div>
+                        </div>
+                    </div>
+                </Col>
+                <Col span="6">
+                    <div class="item-container">
+                        <div class="item">
+                            <div class="title">Repository</div>
+                            <div class="content">200</div>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </CarouselItem>
+    
+    <!--
+        <CarouselItem>
+            <div class="demo-carousel">1</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">2</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">3</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">4</div>
+        </CarouselItem>
+    -->
+    </Carousel>
+  </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                value1: 0,
+                setting: {
+                    dots: 'none',
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+
+    .statistics-container{
+        width:100%;
+        height: 100%;
+        position: relative;
+    }
+    .carousel-container{
+        display:block;
+    }
+    .row{
+        width: 80%;
+        height: 300px;
+        margin:0 auto;
+        display: flex;
+        align-items: center;
+    }
+
+    .item-container{
+        text-align: center;
+        margin: 50px 10px;
+    }
+
+    .item{
+        display: flex;
+        flex-direction:column;
+        padding: 50px 0;
+        background: red;
+       
+    }
+</style>
