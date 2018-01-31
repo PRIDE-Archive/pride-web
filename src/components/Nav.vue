e771ced1b7037b3580c057f90876eede58cda13b
shabai517 <shabai517@163.com>
2018-01-31 15:21:42 +0000

    NAV updated

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,30 +2,30 @@
     <Menu mode="horizontal" :theme="theme1" active-name="1">
         <MenuItem name="1">
             <Icon type="ios-paper"></Icon>
-            内容管理
+            Home
         </MenuItem>
         <MenuItem name="2">
             <Icon type="ios-people"></Icon>
-            用户管理
+            XXXXX
         </MenuItem>
         <Submenu name="3">
             <template slot="title">
                 <Icon type="stats-bars"></Icon>
-                统计分析
+                XXXX
             </template>
             <MenuGroup title="使用">
-                <MenuItem name="3-1">新增和启动</MenuItem>
-                <MenuItem name="3-2">活跃分析</MenuItem>
-                <MenuItem name="3-3">时段分析</MenuItem>
+                <MenuItem name="3-1">XXXXX</MenuItem>
+                <MenuItem name="3-2">XXXXX</MenuItem>
+                <MenuItem name="3-3">XXXXX</MenuItem>
             </MenuGroup>
             <MenuGroup title="留存">
-                <MenuItem name="3-4">用户留存</MenuItem>
-                <MenuItem name="3-5">流失用户</MenuItem>
+                <MenuItem name="3-4">XXXXX</MenuItem>
+                <MenuItem name="3-5">XXXXX</MenuItem>
             </MenuGroup>
         </Submenu>
         <MenuItem name="4">
             <Icon type="settings"></Icon>
-            综合设置
+            XXXXX
         </MenuItem>
     </Menu>
 </template>
