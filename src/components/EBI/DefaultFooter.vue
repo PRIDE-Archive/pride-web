5e3a4a870240848635adb0ecb66b82d512581394
shabai517 <shabai517@163.com>
2018-02-27 11:10:36 +0000

    create default global EBI Footer

---
+++ src/components/EBI/DefaultFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="footer-container">
+        <footer>
+        <div id="global-footer" class="global-footer">
+          <nav id="global-nav-expanded" class="global-nav-expanded row">
+            <!-- Footer will be automatically inserted by footer.js -->
+          </nav>
+          <section id="ebi-footer-meta" class="ebi-footer-meta row">
+            <!-- Footer meta will be automatically inserted by footer.js -->
+          </section>
+        </div>
+      </footer>
+    </div>
+</template>
