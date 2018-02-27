eedf8ced8867bc0894c5484ec3ed6d5a9a5d3375
shabai517 <shabai517@163.com>
2018-02-27 14:14:16 +0000

    add ebi search input

--- src/components/landingpage/Search.vue
+++ src/components/landingpage/Search.vue
@@ -1,5 +1,50 @@
 <template>
     <div class="search-container">
+        <div data-sticky-container>
+          <header id="masthead" class="masthead ebi-masthead" data-sticky data-sticky-on="large" data-top-anchor="content:top" data-btm-anchor="content:bottom">
+            <div class="masthead-inner row">
+              <!-- local-title -->
+              <div class="columns medium-7" id="local-title">
+                <h1><a href="../../" title="Back to [service-name] homepage">EBI Framework</a></h1>
+              </div>
+              <!-- /local-title -->
+              <!-- local-search -->
+              <div class="columns medium-5">
+                <form id="local-search" name="local-search" action="[search-action]" method="post">
+                  <h4>Search this project</h4>
+                  <fieldset class="ebi-fieldset">
+                    <div class="input-group margin-bottom-none">
+                      <input type="text" name="first" id="local-searchbox" placeholder="Search this service" class="input-group-field">
+                      <div class="input-group-button">
+                        <input type="submit" name="submit" value="1" class="button icon icon-functional">
+                      </div>
+                    </div>
+                    <small class="">
+                      <!-- If your search is more complex than just a keyword search, you can link to an Advanced Search -->
+                      <!-- <span class="adv"><a href="../search" id="adv-search" title="Advanced">Advanced</a></span> | -->
+                      <!-- Include some example searchterms - keep them short and few. -->
+                      <a href="[search-url-1]">[search-1]</a>
+                      <a href="[search-url-1]">[search-2]</a>
+                      <a href="[search-url-1]">[search-3]</a>
+                    </small>
+                  </fieldset>
+                </form>
+              </div>
+              <!-- /local-search -->
+              <!-- local-nav -->
+              <nav >
+                <ul id="local-nav" class="dropdown menu float-left" data-description="navigational">
+                  <li><a href="../../">Home</a></li>
+                  <li><a href="../../">Pride</a></li>
+                  <li><a href="../../">Archive</a></li>
+                  <li><a href="../../">Contact</a></li>
+                </ul>
+              </nav>
+              <!-- /local-nav -->
+            </div>
+          </header>
+        </div>
+        <!--
         <Input v-model="keyword" placeholder="search" class="search-input search-pride" @on-keyup.enter.prevent="submit">
             <Select v-model="selected" slot="prepend" style="width: 80px">
                 <Option value="archive">Archive</Option>
@@ -7,6 +52,7 @@
             </Select>
             <Button slot="append" icon="ios-search" @click="submit"></Button>
         </Input>
+        -->
     </div>
 </template>
 <script>
@@ -39,17 +85,24 @@
 </script>
 <style scoped>
     .search-container{
+        /*
         width:100%;
         height: 100%;
         display: flex;
         align-items: center;
         justify-content: center;
-        background: #F0F0F0;
+        background: #F0F0F0;*/
     }
     .search-input{
         width:500px;
     }
     .search-pride .ivu-select-single{
         width: 90px !important;
+    }
+    .ebi-fieldset{
+        border: 0 !important;
+    }
+    .ebi-masthead{
+        background-color: rgb(9, 177, 177) !important;
     }
 </style>
