8486d0d0d745154b7fed36831b149a6c5b223589
shabai517 <shabai517@163.com>
2018-03-01 14:17:26 +0000

    submit data tools updated

--- src/components/landingpage/Tools.vue
+++ src/components/landingpage/Tools.vue
@@ -1,7 +1,48 @@
 <template>
     <div class="tools-container">
-        <div class="title"></div>
-        <Button class="tools-button" @click="submitData">Submit Data</Button>
+        <div class="title">How to submit data</div>
+        <!--<div class="sub-title">PRIDE welcomes direct user submissions of protein and peptide identification/quantification data</div>-->
+        <div class="content">
+            <div class="item">
+                <div class="step-wrapper">
+                    <div class="step">1</div>
+                    <div class="horizontal"></div>
+                </div>
+                <div class="description">Register</div>
+            </div>
+            <div class="item">
+                <div class="step-wrapper">
+                    <div class="step">2</div>
+                    <div class="horizontal"></div>
+                </div>
+                <div class="description">Choose submission type</div>
+            </div>
+            <div class="item">
+                <div class="step-wrapper">
+                    <div class="step">3</div>
+                    <div class="horizontal"></div>
+                </div>
+                <div class="description">Submit the dataset</div>
+            </div>
+            <div class="item">
+                <div class="step-wrapper">
+                    <div class="step">4</div>
+                    <div class="horizontal"></div>
+                </div>
+                <div class="description">Accessing private data</div>
+            </div>
+            <div class="item">
+                <div class="step-wrapper">
+                    <div class="step">5</div>
+                    <!--<div class="horizontal"></div>-->
+                </div>
+                <div class="description">Post-submission steps</div>
+            </div>
+        </div>
+        <div class="tools-button-wrapper">
+            <a class="button tools-button" @click="submitData">Submit Data</a>
+        </div>
+        
     </div>
 </template>
 <script>
@@ -45,16 +86,77 @@
     .tools-container{
         width:100%;
         height: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+        padding:90px 0;
     }
     .search-input{
         width:500px;
     }
     .tools-button{
-        padding: 12px 30px 14px 30px;
-        font-size: 18px;
-        margin: 0 30px;
+        margin: 0 auto;
+        padding: 20px 85px;
+        text-decoration: none;
+        text-transform: uppercase;
+        background-color: rgb(9, 177, 177);
+        font-size: 24px;
     }
+    .title{
+        text-align: center;
+        margin-bottom: 60px;
+        font-size: 28px;
+        font-weight: 400;
+        color: #495060;
+    }
+    .step-wrapper{
+        position: relative;
+    }
+    .step{
+        
+        display: inline-block;
+        width: 80px;
+        height: 80px;
+        line-height: 80px;
+        text-align: center;
+        border-radius: 50%;
+        font-size: 24px;
+        background-color: #0bb1b1;
+        color: white;
+        margin-bottom: 20px;
+    }
+    .description{
+        display: inline-block;
+        font-size: 18px;
+    }
+    .tools-button-wrapper{
+        text-align: center;
+        margin-top: 140px;
+    }
+    .content{
+        text-align: center;
+        margin-top: 120px;
+    }
+    .item{
+        display: inline-flex;
+        flex-direction: column;
+        margin:10px;
+        width: 15%;
+    }
+    .horizontal{
+        border-right: 0;
+        border-top: 2px solid;
+        height: 1px;
+        left: calc(50% - -52px);
+        position: absolute;
+        top: 40px;
+        width: calc(100% - 82px);
+        color: #0bb1b1b5;
+        /*width: 50px;*/
+    }
+    /*
+    .sub-title{
+        text-align: center;
+        margin-bottom: 60px;
+        font-size: 18px;
+        font-weight: normal;
+        color: #04bfbf;
+    }*/
 </style>
