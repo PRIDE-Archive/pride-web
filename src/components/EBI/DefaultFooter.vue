1b30ce49462081f137a96cb4a24091abe806577f
shabai517 <shabai517@163.com>
2018-03-01 14:17:55 +0000

    update footer banner

--- src/components/EBI/DefaultFooter.vue
+++ src/components/EBI/DefaultFooter.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="footer-container">
-        <footer>
-        <div id="global-footer" class="global-footer">
+      <!-- data-color="grey" is the previous settings!!!!!!!!!!!!!!!!!-->
+      <div id="elixir-banner" data-color="#0bb1b1" data-name="This service" data-description="I am a super cool service" data-more-information-link="https://www.elixir-europe.org/about-us/who-we-are/nodes/embl-ebi" data-use-basic-styles="true"></div>
+      <footer>
+        <div id="global-footer" class="global-footer ebi-global-footer" >
           <nav id="global-nav-expanded" class="global-nav-expanded row">
             <!-- Footer will be automatically inserted by footer.js -->
           </nav>
@@ -12,3 +14,10 @@
       </footer>
     </div>
 </template>
+
+<style scoped>
+    .ebi-global-footer{
+      border-top: 0 !important;
+    }
+
+</style>
