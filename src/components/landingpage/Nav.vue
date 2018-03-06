62dcb4cdefe477bac5aac039840093d061edc049
shabai517 <shabai517@163.com>
2018-03-06 16:18:20 +0000

    change search.vue to nav.vue

--- src/components/landingpage/Nav.vue
+++ src/components/landingpage/Nav.vue
@@ -1,15 +1,21 @@
 <template>
-    <div class="search-container">
+    <div class="nav-container">
         <div data-sticky-container>
           <header id="masthead" class="masthead ebi-masthead" data-sticky data-sticky-on="large" data-top-anchor="content:top" data-btm-anchor="content:bottom">
             <div class="masthead-inner row">
               <!-- local-title -->
-              <div class="columns medium-7" id="local-title">
-                <h1><a href="../../" title="Back to [service-name] homepage">EBI Framework</a></h1>
+              <div class="columns medium-2" id="local-title">
+                <div class="pride-logo">
+                  <a href="../../" title="Back to [service-name] homepage"><img src="//www.ebi.ac.uk/pride/resources/logos/pride_logo.jpg" alt="logo" width="128" height="128"></a>
+                </div>
               </div>
               <!-- /local-title -->
+              <div class="columns medium-10">
+                <span id="quickgo-header-subtext"><h1 ng-hide="location.path()!=='/'" class="landingpage-title">PRIDE Cluster and PRIDE Archive</h1></span>
+              </div>
               <!-- local-search -->
-              <div class="columns medium-5">
+              <!--
+              <div class="columns medium-10">
                 <form id="local-search" name="local-search" action="[search-action]" method="post">
                   <h4>Search this project</h4>
                   <fieldset class="ebi-fieldset">
@@ -19,10 +25,11 @@
                         <input type="submit" name="submit" value="1" class="button icon icon-functional">
                       </div>
                     </div>
-                    <small class="">
+                    <small class="">-->
                       <!-- If your search is more complex than just a keyword search, you can link to an Advanced Search -->
                       <!-- <span class="adv"><a href="../search" id="adv-search" title="Advanced">Advanced</a></span> | -->
                       <!-- Include some example searchterms - keep them short and few. -->
+                      <!--
                       <a href="[search-url-1]">[search-1]</a>
                       <a href="[search-url-1]">[search-2]</a>
                       <a href="[search-url-1]">[search-3]</a>
@@ -30,29 +37,20 @@
                   </fieldset>
                 </form>
               </div>
+              -->
               <!-- /local-search -->
               <!-- local-nav -->
               <nav >
                 <ul id="local-nav" class="dropdown menu float-left" data-description="navigational">
                   <li><a href="../../">Home</a></li>
-                  <li><a href="../../">Pride</a></li>
-                  <li><a href="../../">Archive</a></li>
                   <li><a href="../../">Contact</a></li>
+                  <li><a href="../../">About</a></li>
                 </ul>
               </nav>
               <!-- /local-nav -->
             </div>
           </header>
         </div>
-        <!--
-        <Input v-model="keyword" placeholder="search" class="search-input search-pride" @on-keyup.enter.prevent="submit">
-            <Select v-model="selected" slot="prepend" style="width: 80px">
-                <Option value="archive">Archive</Option>
-                <Option value="cluster">Peptidome</Option>
-            </Select>
-            <Button slot="append" icon="ios-search" @click="submit"></Button>
-        </Input>
-        -->
     </div>
 </template>
 <script>
@@ -84,7 +82,7 @@
     }
 </script>
 <style scoped>
-    .search-container{
+    .nav-container{
         /*
         width:100%;
         height: 100%;
@@ -93,16 +91,19 @@
         justify-content: center;
         background: #F0F0F0;*/
     }
-    .search-input{
-        width:500px;
-    }
-    .search-pride .ivu-select-single{
-        width: 90px !important;
-    }
+
     .ebi-fieldset{
         border: 0 !important;
     }
     .ebi-masthead{
         background-color: rgb(9, 177, 177) !important;
+    }
+    .landingpage-title{
+      font-size: 3rem;
+      font-weight:300;
+      font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;
+    }
+    .pride-logo{
+      margin-bottom: 20px;
     }
 </style>
