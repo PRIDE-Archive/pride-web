2a0de54b7b25a09caef468ed140cf90790f92d75
shabai517 <shabai517@163.com>
2018-03-12 15:32:43 +0000

    add echarts components

--- src/main.js
+++ src/main.js
@@ -7,9 +7,19 @@ import iView from 'iview';
 import './assets/my-theme/index.less';
 //import 'iview/dist/styles/iview.css';
 
+import ECharts from 'vue-echarts/components/ECharts'
+// import ECharts modules manually to reduce bundle size
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/component/polar'
+
+
+
 
 Vue.config.productionTip = false
 Vue.use(iView);
+Vue.component('chart', ECharts)
 
 /* eslint-disable no-new */
 new Vue({
