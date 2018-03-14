9917ba53a728390eb4f54b30dd66564ea6c5b6aa
shabai517 <shabai517@163.com>
2018-03-14 12:06:40 +0000

    change news style

--- src/components/landingpage/News.vue
+++ src/components/landingpage/News.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="news-container">
-     <div class="title">News</div>
+     <!--<h1>News</h1>-->
      <Row class="row" type="flex">
             <Col :xs="{ span: 24 }" :sm="{span: 8}" :md="{ span: 8}" :lg="{ span: 8}">
                 <div class="item-container">
-                    <div class="item-icon"><Icon type="document-text" size="80" color="rgb(9, 177, 177)"></Icon></div>
+                    <div class="item-icon"><Icon type="document-text" size="80" color="#656665"></Icon></div>
                     <div class="item-title">
                       <div>Documentation</div>
                     </div>
@@ -12,22 +12,24 @@
                         <div class="title">Documentation Content</div>
                         <div class="content">{{info.document}}</div>
                     </div>
+                    <Button class="news-button">MORE</Button>
                 </div>
             </Col>
             <Col :xs="{ span: 24 }" :sm="{span: 8}" :md="{ span: 8}" :lg="{ span: 8}">
                 <div class="item-container">
-                    <div class="item-icon"><Icon type="social-twitter" size="80" color="rgb(9, 177, 177)"></Icon></div>
+                    <div class="item-icon"><Icon type="social-twitter" size="80" color="#656665"></Icon></div>
                     <div class="item-title">
                       <div>Tweets</div>
                     </div>
                     <div class="item tweet">
-                        <Timeline :id="'pride_ebi'" :widget-class="`tweet-class`" :sourceType="'profile'" :options="{ tweetLimit: '5   ', chrome:'transparent', linkColor:'#0bb1b1', borderColor:'#19b9b9'}"/>
+                        <Timeline :id="'pride_ebi'" :widget-class="`tweet-class`" :sourceType="'profile'" :options="{ tweetLimit: '5   ', chrome:'transparent', linkColor:'#656665', borderColor:'#656665'}"/>
                     </div>
+                    <Button class="news-button">SUBSCRIBE</Button>
                 </div>
             </Col>
             <Col :xs="{ span: 24 }" :sm="{span: 8}" :md="{ span: 8}" :lg="{ span: 8}">
                 <div class="item-container">
-                    <div class="item-icon"><Icon type="social-twitch" size="80" color="rgb(9, 177, 177)"></Icon></div>
+                    <div class="item-icon"><Icon type="social-twitch" size="80" color="#656665"></Icon></div>
                     <div class="item-title">
                       <div>Citation</div>
                     </div>
@@ -35,6 +37,7 @@
                         <div class="title">Citation Content</div>
                         <div class="content">{{info.citation}}</div>
                     </div>
+                    <Button class="news-button">MORE</Button>
                 </div>
             </Col>
      </Row>
@@ -115,14 +118,18 @@
         padding: 90px 0;
     }
     .row{
-        width: 90%;
+        width: 80%;
         margin:0 auto;
         display: flex;
         align-items: center;
     }
     .item-container{
         text-align: center;
-        margin: 15px 30px;
+        margin: 15px;
+        border: 1px solid #ececec;
+        border-radius: 6px;
+
+        padding: 2.78571429em;
     }
     .item{
         display: flex;
@@ -131,6 +138,7 @@
         color: #666;
         height:300px;
         background-color: white;
+        margin-bottom: 20px;
     }
     .item.tweet{
         padding: 10px 0 !important;
@@ -142,17 +150,53 @@
     .item-icon{
         margin-bottom: 12px;
     }
-    .title{
+    .news-button{
+        background: none;
+        border: 1px solid #d3d3d3 !important;
+        padding: 10px 40px;
+        color: #656665;
+        font-weight: 700;
+        font-size: 12px;
+        margin-top: 20px;
+    }
+    h1{
+        color: #656665;
+        font-size: 3.14285714em;
+        font-weight: bold;
+        font-family: Open Sans,Helvetica,Arial,sans-serif;
+        margin-bottom: 20px;
         text-align: center;
-        margin-bottom: 60px;
-        font-size: 28px;
-        font-weight: 400;
     }
     .tweet-class{
-        
         overflow-y: scroll;
     }
     ::-webkit-scrollbar {
         width: 1px;
+    }
+    @media (min-width: 768px) { 
+        .row{
+            width: 750px;
+        }
+        
+    }
+    @media (min-width: 992px) { 
+        .row{
+            width: 970px;
+        }
+        
+    }
+    @media (min-width: 1200px) {
+        .row{
+            width: 1170px;
+        }
+        .item-container{
+            /*margin: 15px 50px;*/
+        }
+    }
+    
+    @media (min-width: 1600px) {
+        .row{
+            width: 1300px;
+        }
     }
 </style>
