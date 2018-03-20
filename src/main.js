926c11c9e3fc2db8d9605be7a6a656e96b8cc28c
shabai517 <shabai517@163.com>
2018-03-20 15:23:25 +0000

    support custom api

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import iView from 'iview';
 import './assets/my-theme/index.less';
+import VueResource  from 'vue-resource'
 //import 'iview/dist/styles/iview.css';
 
 import ECharts from 'vue-echarts/components/ECharts'
@@ -19,6 +20,7 @@ import 'echarts/lib/component/polar'
 
 Vue.config.productionTip = false
 Vue.use(iView);
+Vue.use(VueResource);
 Vue.component('chart', ECharts)
 
 /* eslint-disable no-new */
