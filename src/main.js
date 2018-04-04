bec20e754019c3c84ac3e70f4af7b643190b44a2
shabai517 <shabai517@163.com>
2018-04-04 17:00:57 +0100

    add English into vue

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from 'iview';
+import locale from 'iview/dist/locale/en-US';
 import './assets/my-theme/index.less';
 import VueResource  from 'vue-resource'
 //import 'iview/dist/styles/iview.css';
@@ -19,7 +20,7 @@ import 'echarts/lib/component/polar'
 
 
 Vue.config.productionTip = false
-Vue.use(iView);
+Vue.use(iView, { locale });
 Vue.use(VueResource);
 Vue.component('chart', ECharts)
 
