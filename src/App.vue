15415c14dc57677c3be90038855ef104eb0cfa86
shabai517 <shabai517@163.com>
2018-04-06 00:43:25 +0100

    over write ul and ol from foundation.css

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,25 @@ export default {
   html,body {
       min-width: 657px !important;
   }
+  ul { 
+   list-style-type: disc; 
+   list-style-position: outside; 
+  }
+
+  ol { 
+     list-style-type: decimal; 
+     list-style-position: outside; 
+  }
+  
+  ul ul, ol ul { 
+     list-style-type: circle; 
+     list-style-position: outside; 
+    
+  }
+  ol ol, ul ol { 
+     list-style-type: lower-latin; 
+     list-style-position: outside; 
+     
+  }
 </style>
 
