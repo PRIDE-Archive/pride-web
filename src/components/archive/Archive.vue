fc85c5637e0048740d8c73eb582216b96cb62149
shabai517 <shabai517@163.com>
2018-04-05 14:22:38 +0100

    finish browse data UI

--- src/components/archive/Archive.vue
+++ src/components/archive/Archive.vue
@@ -5,56 +5,63 @@
           <Row class="search-row">
               <Card>
                 <p slot="title">Search</p>
+              
                 <div class="search-container">
-                  <div class="search-input">
-                     <Input v-model="value4" icon="ios-search" placeholder="Enter something..." style="width: 400px"></Input>
-                  </div>
-                  <div class="search-filter">
-                      <div class="filter-wrapper">
-                          <Select v-model="fieldValue" style="width:200px">
-                              <Option v-for="item in fieldSelectors" :value="item.value" :key="item.value">{{ item.label }}</Option>
-                          </Select>
+                    <div class="search-input">
+                       <Input class="archive-search-input" v-model="keyword" icon="ios-search" placeholder="Enter something..." style="width: 100%" size="large"></Input>
+                    </div>
+                    <div class="search-filter">
+                        <div class="filter-wrapper">
+                            <div class="filter-condition">
+                                <Select v-model="fieldValue" style="width:150px" size="small">
+                                    <Option v-for="item in fieldSelectors" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                                </Select>
+                            </div>
+                            <div class="filter-condition">
+                                <Select v-model="containValue" style="width:150px" size="small">
+                                    <Option v-for="item in containSelectors" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                                </Select>
+                            </div>
+                        </div>
+                        <div class="search-button-wrapper">
+                            <a class="button search-button">Search</a>
+                        </div>
+                    </div>
+           
+                    <div class="search-condition-container">
+                      <div class="tag-container">
+                          <Tag type="border" closable>
+                            <span class="search-condition">And</span>
+                            <Dropdown trigger="click">
+                                <a href="javascript:void(0)">
+                                    abcasdsa
+                                    <Icon type="arrow-down-b"></Icon>
+                                </a>
+                                <DropdownMenu slot="list">
+                                    <DropdownItem>And</DropdownItem>
+                                    <DropdownItem>Not</DropdownItem>
+                                    <DropdownItem>Or</DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                          </Tag>
                       </div>
-                      <div class="filter-wrapper">
-                          <Select v-model="containValue" style="width:200px">
-                              <Option v-for="item in containSelectors" :value="item.value" :key="item.value">{{ item.label }}</Option>
-                          </Select>
+                      <div class="tag-container">
+                          <Tag type="border" closable>
+                            <span class="search-condition">And</span>
+                            <Dropdown trigger="click">
+                                <a href="javascript:void(0)">
+                                    abcasdsa
+                                    <Icon type="arrow-down-b"></Icon>
+                                </a>
+                                <DropdownMenu slot="list">
+                                    <DropdownItem>And</DropdownItem>
+                                    <DropdownItem>Not</DropdownItem>
+                                    <DropdownItem>Or</DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                          </Tag>
                       </div>
-                  </div>
-                </div>
-                <div class="search-condition-container">
-                  <div class="tag-container">
-                      <Tag type="border" closable>
-                        <span class="search-condition">And</span>
-                        <Dropdown trigger="click">
-                            <a href="javascript:void(0)">
-                                abcasdsa
-                                <Icon type="arrow-down-b"></Icon>
-                            </a>
-                            <DropdownMenu slot="list">
-                                <DropdownItem>And</DropdownItem>
-                                <DropdownItem>Not</DropdownItem>
-                                <DropdownItem>Or</DropdownItem>
-                            </DropdownMenu>
-                        </Dropdown>
-                      </Tag>
-                  </div>
-                  <div class="tag-container">
-                      <Tag type="border" closable>
-                        <span class="search-condition">And</span>
-                        <Dropdown trigger="click">
-                            <a href="javascript:void(0)">
-                                abcasdsa
-                                <Icon type="arrow-down-b"></Icon>
-                            </a>
-                            <DropdownMenu slot="list">
-                                <DropdownItem>And</DropdownItem>
-                                <DropdownItem>Not</DropdownItem>
-                                <DropdownItem>Or</DropdownItem>
-                            </DropdownMenu>
-                        </Dropdown>
-                      </Tag>
-                  </div>
+                    </div>
                 </div>
               </Card>
           </Row>
@@ -72,13 +79,23 @@
                       -->
                     </span>
                   </p>
-                  <div v-for="item in publicaitionList" class="resource-item" v-bind:key = "item.id">
-                      <a>{{item.id}}</a>
-                      <p>{{item.name}}</p>
-                      <p>{{item.species}}</p>
-                      <p>{{item.description}}</p>
-                      <p>{{item.date}}</p>
-                  </div>
+                  <Card v-for="publicationItem in publicaitionList" class="resource-item" v-bind:key = "publicationItem.id">
+                      <a class="resource-id">{{publicationItem.id}}</a>
+                      <p class="resource-title">{{publicationItem.name}}</p>
+                      <p>Species: {{publicationItem.species}}</p>
+                      <span>Project description: {{publicationItem.description}}</span><a @click="getDetailedResource(publicationItem.id)" class="detailed-resouce">(More)</a>
+                      <p>Made public: {{publicationItem.date}}</p>
+                      <Dropdown class="dataset-wrapper" v-for="datesetItem in publicationItem.dataset">
+                          <a class="button dataset-button" href="javascript:void(0)">
+                             <Icon type="ios-pricetag"></Icon>
+                              {{datesetItem}} 
+                          </a>
+                          <DropdownMenu slot="list">
+                              <DropdownItem>{{datesetItem}}</DropdownItem>
+                          </DropdownMenu>
+                      </Dropdown>
+      
+                  </Card>
                   
                   <div class="page-container">
                     <Page :total="200" :page-size="20" size="small" show-sizer show-total class-name="page"></Page>
@@ -95,6 +112,7 @@
     name: 'archive',
     data(){
       return {
+          keyword:'',
           fieldValue:'Species',
           containValue:'Contains',
           fieldSelectors:[
@@ -163,31 +181,39 @@
           ],
           publicaitionList:[
             {
-              id:'PRD000005',
-              name: 'GPMDB Submission: Aurum Spectra ',
+              id:'PXD008343',
+              completeState:false,
+              name: 'CRISPR/Cas9-mediated knockout and endogenous complex analysis of Cluap1/ IFT38',
               species: 'Homo sapiens (Human)',
-              description:'Project description: Not available (More)',
-              date:'2005-08-16'
+              description:'CRISPR/Cas9-mediated gene-editing allows manipulation of a gene',
+              date:'2018-04-04',
+              dataset:['Biological Dataset']
             },
             {
-              id:'PRD000005',
-              name: 'GPMDB Submission: Aurum Spectra ',
+              id:'PXD008267',
+              completeState:true,
+              name: 'Proteomic profiling of human iPSC-derived pancreatic endocrine cells for the identification of clinical biomarkers ',
               species: 'Homo sapiens (Human)',
-              description:'Project description: Not available (More)',
-              date:'2005-08-16'
+              description:'Great progresses have been made for generating in vitro pluripot',
+              date:'2005-08-16',
+              dataset:['Biological Dataset']
             },
             {
-              id:'PRD000005',
-              name: 'GPMDB Submission: Aurum Spectra ',
+              id:'PXD008970',
+              completeState:false,
+              name: 'Protein citrullination in human tissues',
               species: 'Homo sapiens (Human)',
-              description:'Project description: Not available (More)',
-              date:'2005-08-16'
+              description:'Citrullination is a post-translational modification of arginine',
+              date:'2018-04-04',
+              dataset:['ProteomeTools','Biological Dataset']
             }
           ]
       }
     },
     methods:{
-    
+      getDetailedResource(id){
+
+      }
     },
    
     mounted: function(){
@@ -211,13 +237,10 @@
   .browse-data-container{
     padding: 90px 0;
   }
-  .search-container{
-    display: flex;
-    justify-content:space-between;
-
-  }
   .search-filter{
     display: flex;
+    margin-bottom: 2rem;
+    justify-content: space-between;
   }
   .search-row{
     margin-bottom: 20px;
@@ -225,18 +248,25 @@
   .search-condition{
     display: none;
   }
+  .search-input{
+    text-align: center;
+  }
+  .refine-name{
+    font-size: 12px;
+  }
   .tag-container{
     display: inline-block;
   }
   .page-container{
     text-align: center;
   }
-  .filter-wrapper{
-    margin-left: 10px;
+  .filter-condition{
+    margin-right: 10px;
+    display: inline-block;
   }
   .search-condition-container .ivu-tag{
     overflow: visible !important;
-    z-index: 1000 !important;
+    z-index: 2 !important;
     opacity: 1 !important;
   }
   .search-condition-container a{
@@ -252,11 +282,48 @@
     color: #495060;
     border:none;
   }
-
+  .search-button{
+        padding: 5px 10px;
+        font-size: 12px;
+        width: 100%;
+        margin-bottom: 0;
+        /*padding: 20px 85px;
+        font-size: 24px;*/
+        font-weight: 700;
+        background-color: #5bc0be;
+        border-radius: 3px;
+    }
+    .resource-item{
+      margin-bottom: 20px;
+    }
+    .resource-id{
+      font-size: 14px;
+    }
+    .resource-title{
+      font-weight: bold;
+    }
+    .detailed-resouce{
+      margin-left: 5px;
+    }
+    .dataset-button{
+        padding: 2px 3px;
+        font-size: 12px;
+        margin-bottom: 0;
+        /*padding: 20px 85px;
+        font-size: 24px;*/
+        background-color: #5bc0be;
+        border-radius: 3px;
+    }
+    .dataset-wrapper{
+      margin-right: 5px;
+    }
 </style>
 
 <style>
     .page .ivu-select-dropdown-list{
-    margin-left: 0 !important;
-  }
+      margin-left: 0 !important;
+    }
+    .archive-search-input input{
+      border-radius: 3px;
+    }
 </style>
