c16f7beb9eea3881070722587a50da88ff7d6190
shabai517 <shabai517@163.com>
2018-04-06 10:26:10 +0100

    backup markdown back api

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var EditPageController = require("./EditPageController.js");
+var MarkdownController = require("./MarkdownController.js");
 
 router.use("/api/editpage",EditPageController);
+router.use("/api/markdownquery",MarkdownController);
 
 module.exports = router;
