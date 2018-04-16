56af8e09379de84abc62f0dfab5ef26eab1423c6
shabai517 <shabai517@163.com>
2018-04-16 09:33:01 +0100

    add test component

---
+++ src/components/test.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="test-container">
+       
+    </div>
+</template>
+<script>
+    
+    export default {
+        data () {
+        
+        },
+       
+        methods:{
+            markdownQuery(){
+                this.$http
+                  .get(this.markdownURL)
+                  .then(function(res){
+                    console.log(res);
+                    this.source = res.body;
+                  },function(err){
+                   
+                  }); 
+            }
+        },
+       
+    }
+</script>
+<style>
+ 
+</style>
+<style scoped>
+    .test-container{
+        width:100%;
+        height: 100%;
+    }
+</style>
