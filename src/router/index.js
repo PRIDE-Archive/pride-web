82e277b70b3691659a723a40c9305b8eaec175e1
shabai517 <shabai517@163.com>
2018-04-16 15:40:39 +0100

    index

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 //import Index from '@/components/landingpage/Index'
-
+const Test = resolve => require(['@/components/Test'], resolve);
 const LandingPage = resolve => require(['@/components/landingpage/LandingPage'], resolve);
 const SubmitdataPage = resolve => require(['@/components/landingpage/SubmitdataPage'], resolve);
-const SubmitStep1 = resolve => require(['@/components/landingpage/SubmitStep1'], resolve);
-const SubmitStep2 = resolve => require(['@/components/landingpage/SubmitStep2'], resolve);
-const SubmitStep3 = resolve => require(['@/components/landingpage/SubmitStep3'], resolve);
-const SubmitStep4 = resolve => require(['@/components/landingpage/SubmitStep4'], resolve);
-const SubmitStep5 = resolve => require(['@/components/landingpage/SubmitStep5'], resolve);
 const Archive = resolve => require(['@/components/archive/Archive'], resolve);
 const Citation = resolve => require(['@/components/landingpage/Citation'], resolve);
 
@@ -28,31 +23,6 @@ export default new Router({
       component: SubmitdataPage
     },
     {
-      path: '/submitstep1',
-      name: 'submitstep1',
-      component: SubmitStep1
-    },
-    {
-      path: '/submitstep2',
-      name: 'submitstep2',
-      component: SubmitStep2
-    },
-    {
-      path: '/submitstep3',
-      name: 'submitstep3',
-      component: SubmitStep3
-    },
-    {
-      path: '/submitstep4',
-      name: 'submitstep4',
-      component: SubmitStep4
-    },
-     {
-      path: '/submitstep5',
-      name: 'submitstep5',
-      component: SubmitStep5
-    },
-    {
       path: '/archive',
       name: 'archive',
       component: Archive
@@ -61,6 +31,11 @@ export default new Router({
       path: '/citation',
       name: 'citation',
       component: Citation
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test
     },
   ],
 })
