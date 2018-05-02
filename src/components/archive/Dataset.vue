5bcf79dab84e397cc775258a5db9dcc0e40fbaf2
shabai517 <shabai517@163.com>
2018-05-02 14:00:43 +0100

    dataset update

--- src/components/archive/Dataset.vue
+++ src/components/archive/Dataset.vue
@@ -13,6 +13,7 @@
             
                <h2 class="project-title">Project PXD006887</h2>
                 <div class="tags">
+                  <!--
                     <span class="type-tag-wrapper">
                       <Icon class="type-tag" type="ios-pricetag-outline"></Icon>
                       <a>Biological Dataset</a>
@@ -20,6 +21,7 @@
                     <span>
                       <Icon class="download-tag" type="ios-download-outline"></Icon><a>Download</a>
                     </span>
+                  -->
                 </div>
             </Col>
           </Row>
@@ -43,6 +45,47 @@
                           <p>07/07/2017</p>
                           <div class="summary-content-header">Publication Date</div>
                           <p>01/05/2018</p>
+                    </Card>
+                    <Card class="card">
+                       <p slot="title">Property</p>
+                       <div class="property">
+                          <div class="property-col">
+                            <div class="property-row">
+                                <div class="summary-content-header">Species</div>
+                                <a>Homo sapiens (Human)</a>
+                            </div>
+                            <div class="property-row">
+                                <div class="summary-content-header">Tissue</div>
+                                <p>kidney</p>
+                            </div>
+                            <div class="property-row">
+                                <div class="summary-content-header">Instrument</div>
+                                <a>Q Exactive</a>
+                            </div>
+                            <div class="property-row">
+                                <div class="summary-content-header">Software</div>
+                                <a>kidney</a>
+                            </div>
+                          </div>
+                          <div class="property-col">
+                            <div class="property-row">
+                                <div class="summary-content-header">Tissue</div>
+                                <p>Not available</p>
+                            </div>
+                            <div class="property-row">
+                                <div class="summary-content-header">Modification</div>
+                                <a>No PTMs are included in the dataset</a>
+                            </div>
+                            <div class="property-row">
+                                <div class="summary-content-header">Quantification</div>
+                                <p>Not available</p>
+                            </div>
+                            <div class="property-row">
+                                <div class="summary-content-header">Experiment Type</div>
+                                <p>Shotgun proteomics</p>
+                            </div>
+                          </div>
+                       </div>
                     </Card>
                     <Card class="card">
                         <p slot="title">Publication</p>
@@ -88,24 +131,34 @@
                 -->
               </Col>
               <Col span="8">
+                   <Card class="card">
+                       <p slot="title"> <i class="fas fa-download icon-tag"></i>Download</p>
+                       <div class="filter-wrapper">
+                           <div class="summary-content-header">Filter</div>
+                           <Select v-model="model1" size="small" style="width:100px">
+                              <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                           </Select>
+                       </div>
+                       <div class="download-list-wrapper">
+                         <div class="summary-content-header">List</div>
+                         <div class="download-list">
+                           <Table border ref="selection" :columns="columns4" :data="data1"></Table>
+                           <Button class= "download-button">Download</Button>
+                         </div>
+                       </div>
+                  </Card>
                   <Card class="card">
-                       <p slot="title">Description</p>
-                       <div class="summary-content-header">Species</div>
-                       <a>Homo sapiens (Human)</a>
-                       <div class="summary-content-header">Tissue</div>
-                       <p>kidney</p>
-                       <div class="summary-content-header">Instrument</div>
-                       <a>Q Exactive</a>
-                       <div class="summary-content-header">Software</div>
-                       <a>kidney</a>
-                       <div class="summary-content-header">Tissue</div>
-                       <p>Not available</p>
-                       <div class="summary-content-header">Modification</div>
-                       <a>No PTMs are included in the dataset</a>
-                       <div class="summary-content-header">Quantification</div>
-                       <p>Not available</p>
-                       <div class="summary-content-header">Experiment Type</div>
-                       <p>Shotgun proteomics</p>
+                       <p slot="title"><i class="fas fa-link icon-tag"></i>Similar Studies</p>
+                       <div class="list-wrapper">
+                        <ul>
+                          <li><a>Homo sapiens (Human) XXXXXXX  XXXXXX XXXXX XXXX XXXXX</a></li>
+                          <li><a>Homo sapiens (Human) XXXXXXX  XXXXXX XXXXX XXXX XXXXX</a></li>
+                          <li><a>Homo sapiens (Human) XXXXXXX  XXXXXX XXXXX XXXX XXXXX</a></li>
+                          <li><a>Homo sapiens (Human) XXXXXXX  XXXXXX XXXXX XXXX XXXXX</a></li>
+                          <li><a>Homo sapiens (Human) XXXXXXX  XXXXXX XXXXX XXXX XXXXX</a></li>
+                        </ul>
+                       </div>
+                      </p>
                   </Card>
               </Col>
           </Row>
@@ -120,7 +173,72 @@
     name: 'archive',
     data(){
       return {
-         
+         columns4: [
+              {
+                  type: 'selection',
+                  width: 40,
+                  align: 'center'
+              },
+              {
+                  title: 'Name',
+                  key: 'name',
+                  ellipsis:true
+              },
+              {
+                  title: 'Size (M)',
+                  width: 80,
+                  key: 'size',
+                  sortable: true,
+                    align:'center'
+              },
+            
+          ],
+          data1: [
+              {
+                  name: '41598_2018_23766_MOESM1_ESM.doc',
+                  size: 1312.606,
+                 
+              },
+              {
+                  name: '41598_2018_23766_MOESM1_ESM.doc',
+                  size: 1312.606,
+              },
+              {
+                  name: '41598_2018_23766_MOESM1_ESM.doc',
+                  size: 1312.606,
+              },
+              {
+                  name: '41598_2018_23766_MOESM1_ESM.doc',
+                  size: 1312.606,
+              }
+          ],
+          cityList: [
+                    {
+                        value: 'New York',
+                        label: 'New York'
+                    },
+                    {
+                        value: 'London',
+                        label: 'London'
+                    },
+                    {
+                        value: 'Sydney',
+                        label: 'Sydney'
+                    },
+                    {
+                        value: 'Ottawa',
+                        label: 'Ottawa'
+                    },
+                    {
+                        value: 'Paris',
+                        label: 'Paris'
+                    },
+                    {
+                        value: 'Canberra',
+                        label: 'Canberra'
+                    }
+                ],
+                model1: ''
       }
     },
     components: {
@@ -164,22 +282,14 @@
   .tags a:hover{
     color: #5bc0be;;
   }
-  .download-tag{
-    font-size: 21px;
+  .icon-tag{
+    font-size: 12px;
     margin-right: 5px;
-  }
-  .type-tag{
-    font-size: 17px;
-    margin-right: 1px;
-  }
-  .type-tag-wrapper{
-    margin-right: 10px;
   }
   .summary-content-header{
     font-size: 14px;
     color: #5bc0be;
     font-weight: bold;
-    margin-top: 10px;
   }
   .card p{
     text-align: justify;
@@ -189,8 +299,63 @@
   }
   .card a{
       color: #495060;
+      border-bottom-style: none;
   }
   .card a:hover{
         color: #5bc0be;
+  }
+  .card .property{
+    display: flex;
+  }
+  .card .property .property-col{
+    display: flex;
+    flex:1;
+    flex-direction:column;
+  }
+  .card .property .property-col:not(:last-child) {
+    margin-right: 40px;
+  }
+  .card .property .property-col .property-row{
+    display: flex;
+    padding: 12px 0;
+    justify-content: space-between;
+    border-bottom: 1px solid #e7e7e7;
+  }
+  .list-wrapper{
+    padding-left: 20px;
+  }
+  .download-button{
+    padding: 6px 8px;
+    font-size: 12px;
+    margin-top: 10px;
+    font-weight: 700;
+    background-color: #5bc0be;
+    border-radius: 3px;
+    color: #f8f8f8;
+    border-bottom-style:none;
+    display: inline-block;
+  }
+  .download-button:hover{
+    opacity: .8;
+  }
+  .download-list-wrapper{
+    margin-top: 10px;
+  }
+</style>
+<style>
+  .card .ivu-card-body table{
+    margin-bottom: 0;
+  }
+  .card .ivu-card-body .ivu-checkbox-wrapper{
+    margin-right: 0px;
+  }
+  .card .ivu-card-body .ivu-table-cell{
+    padding: 0 5px;
+  }
+  table tbody td{
+    padding: 0px;
+  }
+  table thead th{
+    padding: 0px;
   }
 </style>
