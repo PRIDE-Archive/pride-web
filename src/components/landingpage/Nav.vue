7d480e9c117b0cb26cdb591185751feec4e1c3f1
shabai517 <shabai517@163.com>
2018-05-30 15:52:36 +0100

    logo updated

--- src/components/landingpage/Nav.vue
+++ src/components/landingpage/Nav.vue
@@ -6,7 +6,7 @@
               <!-- local-title -->
               <div class="columns medium-5" id="local-title">
                 <div class="pride-logo">
-                  <a href="../../" title="Back to [service-name] homepage"><img src="/static/logo/PRIDE_Concept_2.png" alt="logo" width="300" height="300"></a>
+                  <a href="../../" title="Back to [service-name] homepage"><img src="/static/logo/PRIDE_logo_final1.png" alt="logo" width="300" height="300"></a>
                 </div>
               </div>
               <!-- /local-title -->
