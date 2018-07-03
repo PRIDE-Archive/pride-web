847d52de938ae1d5cea7b606121d7bc6eba3137c
shabai517 <shabai517@163.com>
2018-07-03 12:15:50 +0100

    update highlights and some visualization library imported

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import iView from 'iview';
 import locale from 'iview/dist/locale/en-US';
 import './assets/my-theme/index.less';
 import VueResource  from 'vue-resource'
+import TextHighlight from 'vue-text-highlight';
 //import 'iview/dist/styles/iview.css';
 
 import ECharts from 'vue-echarts/components/ECharts'
@@ -14,19 +15,27 @@ import ECharts from 'vue-echarts/components/ECharts'
 import 'echarts/lib/chart/bar'
 import 'echarts/lib/component/tooltip'
 import 'echarts/lib/chart/line'
+import 'echarts/lib/chart/sunburst'
+import 'echarts/lib/chart/sankey'
 import 'echarts/lib/component/polar'
 import 'echarts/lib/chart/pie'
 import 'echarts/lib/component/legend'
 import 'echarts/lib/component/legend/ScrollableLegendModel.js'
 import 'echarts/lib/component/legend/ScrollableLegendView.js'
 import 'echarts/lib/component/legend/scrollableLegendAction.js'
+//import 'echarts/lib/component/visualMap'//these two used for dividing the data accoridng to Y
+//import 'echarts/lib/component/visualMapContinuous'//these two used for dividing the data accoridng to Y
+import 'echarts/lib/component/dataZoom.js'
+import 'echarts/lib/chart/map'
+import 'echarts/map/json/world.json'
+import 'echarts/map/js/world.js'
 
 import VueMarkdown from 'vue-markdown';
 import ReadMore from 'vue-read-more';
  
 
 Vue.component('vue-markdown', VueMarkdown);
-
+Vue.component('text-highlight', TextHighlight);
 Vue.config.productionTip = false
 Vue.use(iView, { locale });
 Vue.use(VueResource);
