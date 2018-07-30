dba138d5f28fac27db3c4b0a949939d972aaec54
shabai517 <shabai517@163.com>
2018-07-30 12:11:19 +0100

    add Sankey visualization and make the item clickable

--- src/components/landingpage/statistics_chart/Sankey.vue
+++ src/components/landingpage/statistics_chart/Sankey.vue
@@ -1,5 +1,5 @@
 <template>
-    <chart class="test" :options="options" :auto-resize="true"></chart>
+    <chart class="test" :options="options" :auto-resize="true" @click="itemClick"></chart>
 </template>
 <script>
 export default {
@@ -26,31 +26,51 @@ export default {
               links: [{
                   source: 'a',
                   target: 'a1',
-                  value: 5
+                  value: 5,
+                  url:'http://www.google.com'
               }, {
                   source: 'a',
                   target: 'a2',
-                  value: 3
+                  value: 3,
+                  url:'http://www.google.com'
               }, {
                   source: 'b',
                   target: 'b1',
-                  value: 8
+                  value: 8,
+                  url:'http://www.google.com'
               }, {
                   source: 'a',
                   target: 'b1',
-                  value: 3
+                  value: 3,
+                  url:'http://www.google.com'
               }, {
                   source: 'b1',
                   target: 'a1',
-                  value: 1
+                  value: 1,
+                  url:'http://www.google.com'
               }, {
                   source: 'b1',
                   target: 'c',
-                  value: 2
+                  value: 2,
+                  url:'http://www.google.com'
               }]
           }
       }
     }
+  },
+  methods:{
+      setOptions(data){
+        console.log('sankey',data);
+      },
+      itemClick(e){
+        window.open(e.data.url);
+      }
+  },
+  created(){
+    this.$bus.$on('show-sankey', this.setOptions);
+  },
+  beforeCreate:function(){
+    this.$bus.$off('show-sankey');
   }
 }
 </script>
