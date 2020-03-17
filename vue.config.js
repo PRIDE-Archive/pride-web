ddfd6bd93d61f01b2703d9732b91978081cba3d8
shabai517 <shabai517@163.com>
2020-03-17 15:06:16 +0000

    test syntax

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 	outputDir:'dist/pride',
+	lintOnSave: false,
 	productionSourceMap: false,
 	devServer: {
 		proxy: {
