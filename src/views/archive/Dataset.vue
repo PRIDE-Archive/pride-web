50512a9838f30e728596b45ca91d1dbd81777d85
shabai517 <shabai517@163.com>
2020-05-19 18:20:28 +0800

    add hint for private project
    1. change the query of “getMSRunTableData”
    2. add hint modal

--- src/views/archive/Dataset.vue
+++ src/views/archive/Dataset.vue
@@ -944,9 +944,19 @@
                   }
                   this.publications.push(item);
                 }
-
+                this.getMSRunTableData()
             },function(err){
-                this.$Message.error({content:'No results', duration:3});
+                if(err.bodyText.match('not in the database')){
+                    this.$Modal.warning({
+                        title: 'Not Public Project',
+                        content: '<p>The requested project is not public</p>',
+                        onOk: () => {
+                            this.$Message.info('Clicked ok');
+                        },
+                    });
+                }
+                else
+                  this.$Message.error({content:'No results', duration:3});
             });
       },
       getMSRunTableData(){
@@ -1263,7 +1273,6 @@
         //this.queryAssay();
         this.queryArchiveProjectFiles();
         this.querySimilarity();
-        this.getMSRunTableData();
         this.getProteinEvidences();
 
         //this.initAsperaConnect();
