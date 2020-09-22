a5d067891a7cd926df9882293401d4d3ea11eda4
shabai517 <shabai517@163.com>
2020-09-23 00:02:02 +0800

    support dev production

--- src/store.js
+++ src/store.js
@@ -4,12 +4,23 @@ import Vuex from 'vuex';
 //只要是plugin,都可以用这种use的方法去应用。
 Vue.use(Vuex);
 
+console.log(process.env.NODE_ENV)
+console.log(location.href)
+
+let mode = ''
+if(process.env.NODE_ENV == 'development')
+	mode = 'dev'
+else if(location.href.match(/www\.ebi\.ac\.uk\/pride/))
+	mode = 'prod'
+else
+	mode = 'prodev'
+
 export default new Vuex.Store({
 	state:{
-		baseURL: process.env.NODE_ENV == 'production'?'/pride':'',
+		baseURL: mode == 'production'?'/pride':'',
 		//baseApiURL: location.hostname.match(/localhost/)?'//ves-pg-41:9020':'//wwwdev.ebi.ac.uk/pride/ws/archive',
-		baseApiURL: process.env.NODE_ENV.match(/production|prod-api/) ? 'https://www.ebi.ac.uk/pride/ws/archive/v2':'https://www.ebi.ac.uk/pride/ws/archive/v2',
-		basePrivateURL: process.env.NODE_ENV.match(/production|prod-api/) ? 'https://www.ebi.ac.uk/pride/private/ws/archive/v2':'https://www.ebi.ac.uk/pride/private/ws/archive/v2',
+		baseApiURL: (mode == 'dev' || mode == 'prod') ? 'https://www.ebi.ac.uk/pride/ws/archive/v2' : 'https://wwwdev.ebi.ac.uk/pride/ws/archive/v2',
+		basePrivateURL: (mode == 'dev' || mode == 'prod') ? 'https://www.ebi.ac.uk/pride/private/ws/archive/v2' : 'https://www.ebi.ac.uk/pride/private/ws/archive/v2',
 		username: '',
 		token:''
 
