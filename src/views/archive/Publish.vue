3a2948d7771cc44d8a8f6c6b8712e210e89b1132
shabai517 <shabai517@163.com>
2022-06-13 18:31:57 +0800

    refine repeat check

--- src/views/archive/Publish.vue
+++ src/views/archive/Publish.vue
@@ -63,7 +63,8 @@
     export default {
         data () {
              const validatePass = async (rule, value, callback) => {
-                if (!value) {  
+              // console.log('value',value)
+                if (!this.formInlinePublish.id) {  
                     let err = this.formInlinePublish.title == 'PubMedID' ? 'Please input PubMed' : 'Please input DOI'
                     callback(new Error(err));
                 } 
@@ -138,88 +139,88 @@
         },
         methods:{
             publish(name){
-                this.$Modal.confirm({
-                    title: 'Publish Data',
-                    content: '<p>Do you want to publish this dataset?</p>',
-                    onOk: () => {
-                         this.submit(name)
-                    },
-                    onCancel: () => {
+              this.$refs[name].validate((valid) => {
+                  if (!valid) {
+                    this.$Message.error({ content: 'Fill all required items' });
+                    return
+                  }
+                  this.$Modal.confirm({
+                      title: 'Publish Data',
+                      content: '<p>Do you want to publish this dataset?</p>',
+                      onOk: () => {
+                           this.submit(name)
+                      },
+                      onCancel: () => {
 
-                    }
-                });
+                      }
+                  });
+              })
             },
             submit(name){
-                this.$refs[name].validate((valid) => {
-                      console.log('valid',valid)
-                      if (!valid) {
-                        this.$Message.error({ content: 'Fill all required items' });
-                        return
-                      }
-                      // this.$Spin.show({
-                      //   render: (h) => {
-                      //     return h('div', [
-                      //       h('Icon', {
-                      //         'class': 'demo-spin-icon-load',
-                      //         props: {
-                      //           type: 'load-b',
-                      //           size: 18
-                      //         }
-                      //       }),
-                      //       h('div', 'Please wait...')
-                      //     ])
-                      //   }
-                      // });
-                      let query = {}; //two items: publishJustification and pubmedId/doi
-                      query.PublishProjectRequest = {
-                        publishJustification: this.formInlinePublish.reason,
-                      }
+                // this.$Spin.show({
+                //   render: (h) => {
+                //     return h('div', [
+                //       h('Icon', {
+                //         'class': 'demo-spin-icon-load',
+                //         props: {
+                //           type: 'load-b',
+                //           size: 18
+                //         }
+                //       }),
+                //       h('div', 'Please wait...')
+                //     ])
+                //   }
+                // });
+                let query = {}; //two items: publishJustification and pubmedId/doi
+                query.PublishProjectRequest = {
+                  publishJustification: this.formInlinePublish.reason,
+                }
 
-                      if(this.formInlinePublish.title == 'PubMedID')
-                        query.PublishProjectRequest.pubmedId = this.formInlinePublish.id 
-                      else if(this.formInlinePublish.title == 'DOI')
-                        query.PublishProjectRequest.doi = this.formInlinePublish.id //the value is the same, only the obj name is different
+                if(this.formInlinePublish.title == 'PubMedID')
+                  query.PublishProjectRequest.pubmedId = this.formInlinePublish.id 
+                else if(this.formInlinePublish.title == 'DOI')
+                  query.PublishProjectRequest.doi = this.formInlinePublish.id //the value is the same, only the obj name is different
 
-                      let api;
-                      if(this.$route.query && this.$route.query.r == 'self')
-                          api = this.publishSelfAPI;
-                      else
-                          api = this.publishOtherAPI;
-                       // this.$http
-                       //      .post(api+'/'+this.formInlinePublish.accession,query,{
-                       //        headers: {
-                       //          'Authorization':'Bearer '+ localStorage.getItem('token')
-                       //        }
-                       //      })
-                       //      .then(function(res){
-                       //            this.$Spin.hide()
-                       //            this.$refs[name].resetFields();
-                       //            this.publishModel = true
-                       //            this.$Message.success({ content: 'Publish Successfully!',duration:'5'});
-                       //            // this.$router.push({name:'dataset',params:{id:id}}); TODO: put the id to redirect to the dataset page.
-                       //      },function(err){
-                       //          console.log('errerrerr',err)
-                       //          this.$Spin.hide()
-                       //          if(err.body){
-                       //              if(err.body.hasOwnProperty('message')){
-                       //                  if(err.body.message == 'Supplied Token is not a valid JWT token')
-                       //                     this.$Message.error({ content: 'Invalid Token, Please log in!',duration:'10'});
-                       //                  else
-                       //                    this.$Message.error({ content: 'Publish Error, contact pride-support',duration:'10'});
-                       //              }
-                       //              else{
-                       //                  let errArray = err.body;
-                       //                  if(errArray[0].hasOwnProperty('defaultMessage'))
-                       //                    this.$Message.error({ content: errArray[0].defaultMessage,duration:'10'});
-                       //                  else
-                       //                    this.$Message.error({ content: 'Unknow Error, contact pride-support',duration:'10'});
-                       //              }
-                       //          }
-                       //          else{
-                       //              this.$Message.error({ content: 'Error: ' + err.bodyText?err.bodyText:'', duration:'10'});
-                       //          }
-                       //      });
-                })
+                let api;
+                if(this.$route.query && this.$route.query.r == 'self')
+                    api = this.publishSelfAPI;
+                else
+                    api = this.publishOtherAPI;
+                 // this.$http
+                 //      .post(api+'/'+this.formInlinePublish.accession,query,{
+                 //        headers: {
+                 //          'Authorization':'Bearer '+ localStorage.getItem('token')
+                 //        }
+                 //      })
+                 //      .then(function(res){
+                 //            this.$Spin.hide()
+                 //            this.$refs[name].resetFields();
+                 //            this.publishModel = true
+                 //            this.$Message.success({ content: 'Publish Successfully!',duration:'5'});
+                 //            // this.$router.push({name:'dataset',params:{id:id}}); TODO: put the id to redirect to the dataset page.
+                 //      },function(err){
+                 //          console.log('errerrerr',err)
+                 //          this.$Spin.hide()
+                 //          if(err.body){
+                 //              if(err.body.hasOwnProperty('message')){
+                 //                  if(err.body.message == 'Supplied Token is not a valid JWT token')
+                 //                     this.$Message.error({ content: 'Invalid Token, Please log in!',duration:'10'});
+                 //                  else
+                 //                    this.$Message.error({ content: 'Publish Error, contact pride-support',duration:'10'});
+                 //              }
+                 //              else{
+                 //                  let errArray = err.body;
+                 //                  if(errArray[0].hasOwnProperty('defaultMessage'))
+                 //                    this.$Message.error({ content: errArray[0].defaultMessage,duration:'10'});
+                 //                  else
+                 //                    this.$Message.error({ content: 'Unknow Error, contact pride-support',duration:'10'});
+                 //              }
+                 //          }
+                 //          else{
+                 //              this.$Message.error({ content: 'Error: ' + err.bodyText?err.bodyText:'', duration:'10'});
+                 //          }
+                 //      });
+              
             },
             publishModalOk(){
               this.publishModel=false
@@ -274,8 +275,10 @@
               }
               return result
             },
-            validTypeChange(){
-              console.log('validTypeChange',this.formInlinePublish.title)
+            async validTypeChange(){
+                // if (this.formInlinePublish.id) {  
+                //     await validatePass()
+                // } 
             }
           },
           mounted:function(){
