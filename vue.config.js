094b51acf1fd29f88c031e074c16fda04e55c490
shabai517 <shabai517@163.com>
2022-08-23 17:45:22 +0800

    clean vue.config code

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 module.exports = {
 	outputDir:'dist/pride',
 	publicPath: process.env.NODE_ENV === 'production' ? '/pride/' : '/',
-	// indexPath: process.env.NODE_ENV === 'production' ? '../dist/pride/index.html' : '/',
-	// assetsDir: process.env.NODE_ENV === 'production' ? '../dist/pride' : '/',
 	lintOnSave: false,
 	productionSourceMap: false,
 	devServer: {
